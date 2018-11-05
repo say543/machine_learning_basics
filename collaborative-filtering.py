5d61164aeceb5cdb6b1b2341cfceaf76df2a8146
Morris <morris@Kents-MacBook-Pro.local>
2018-11-04 23:48:54 -0800

    add comment until calc_similarity

--- collaborative-filtering.py
+++ collaborative-filtering.py
@@ -7,85 +7,135 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 
-# custom
-#import recommendation_helpers
-# calculating mean squere error
-def get_mse(pred, actual):
-    # Ignore nonzero terms.
-    pred = pred[actual.nonzero()].flatten()
-    actual = actual[actual.nonzero()].flatten()
-    return mean_squared_error(pred, actual)
 
-# cross validation
-def train_test_split(ratings):
-    test = np.zeros(ratings.shape)
-    train = ratings.copy()
-    for user in xrange(ratings.shape[0]):
-        test_ratings = np.random.choice(ratings[user, :].nonzero()[0], 
+
+
+class recommendation_helpers:
+    
+    def __init__(self):
+        pass
+    # custom
+    #import recommendation_helpers
+    # calculating mean squere error
+    def get_mse(pred, actual):
+        # Ignore nonzero terms.
+        pred = pred[actual.nonzero()].flatten()
+        actual = actual[actual.nonzero()].flatten()
+        return mean_squared_error(pred, actual)
+
+    # cross validation
+    # ? can we user from sklearn.model_selection import train_test_split to do
+    # probaly not becasue it cannot be selected row-based
+    # ? renaming ratings to user_item_matrix is better
+    def train_test_split(ratings):
+
+        # ratings  = user_item_matrix = np.zeros((n_users, n_items)), value store rating
+
+        test = np.zeros(ratings.shape)
+        train = ratings.copy()
+        for user in xrange(ratings.shape[0]):
+
+            # choose 10 rating for a single users
+            # ratings[user, :].nonzero()[0] : get index of rating non zero for a special user
+            # test_ratings. 10 of non-zeros ratings , these rating will be used as testing, copy to test array
+            # in original array set it as zero
+            test_ratings = np.random.choice(ratings[user, :].nonzero()[0], 
                                         size=10, 
                                         replace=False)
-        train[user, test_ratings] = 0.
-        test[user, test_ratings] = ratings[user, test_ratings]
+            train[user, test_ratings] = 0.
+            test[user, test_ratings] = ratings[user, test_ratings]
         
-    # Test and training are truly disjoint
-    assert(np.all((train * test) == 0)) 
-    return train, test
+        # Test and training are truly disjoint
+        assert(np.all((train * test) == 0)) 
+        return train, test
 
-# measure distance L2
-def calc_similarity(ratings, kind='user', epsilon=1e-9):
-    # epsilon -> small number for handling dived-by-zero errors
-    if kind == 'user':
-        sim = ratings.dot(ratings.T) + epsilon
-    elif kind == 'item':
-        sim = ratings.T.dot(ratings) + epsilon
-    norms = np.array([np.sqrt(np.diagonal(sim))])
-    return (sim / norms / norms.T)
+    # measure distance L2
+    # we use cosine similarity algorithm)
+    # ? renaming ratings to user_item_matrix is better
+    def calc_similarity(ratings, kind='user', epsilon=1e-9):
+
+        # ratings  = user_item_matrix = np.zeros((n_users, n_items)), value store rating
+
+        # epsilon -> small number for handling dived-by-zero errors
+        if kind == 'user':
+            # sim : (n_users, n_users)
+            sim = ratings.dot(ratings.T) + epsilon
+        elif kind == 'item':
+            sim = ratings.T.dot(ratings) + epsilon
+
+        # diagonal is user A to user A s square, same user
+        # extract it as array , sqrt it as a single column vector
+        # [] add one dimention so becomes row array
+        # norms: [1, n_users]
+        norms = np.array([np.sqrt(np.diagonal(sim))])
+
+        # = sum / (norms.dot(norms.T) )
+        # by test
+        #  norms / norms.T = [n_users, n_users ]
+        # for each element in norms it divides n_users element in norms.T and create [n_users, 1]
+        # ? i prefer to go wtih  sim /  norms.T.dot(norms) to form [n_users, n_users]
+
+        return (sim / norms / norms.T)
 
 
 
-def predict_simple(ratings, similarity, kind='user'):
-    if kind == 'user':
-        return similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
-    elif kind == 'item':
-        return ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    def predict_simple(ratings, similarity, kind='user'):
+        if kind == 'user':
+            return similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
+        elif kind == 'item':
+            return ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
 
-def predict_topk(ratings, similarity, kind='user', k=40):
-    pred = np.zeros(ratings.shape)
-    if kind == 'user':
-        for i in xrange(ratings.shape[0]):
-            top_k_users = [np.argsort(similarity[:,i])[:-k-1:-1]]
-            for j in xrange(ratings.shape[1]):
-                pred[i, j] = similarity[i, :][top_k_users].dot(ratings[:, j][top_k_users]) 
-                pred[i, j] /= np.sum(np.abs(similarity[i, :][top_k_users]))
-    if kind == 'item':
-        for j in xrange(ratings.shape[1]):
-            top_k_items = [np.argsort(similarity[:,j])[:-k-1:-1]]
+    def predict_topk(ratings, similarity, kind='user', k=40):
+        pred = np.zeros(ratings.shape)
+        if kind == 'user':
             for i in xrange(ratings.shape[0]):
-                pred[i, j] = similarity[j, :][top_k_items].dot(ratings[i, :][top_k_items].T) 
-                pred[i, j] /= np.sum(np.abs(similarity[j, :][top_k_items]))        
+                top_k_users = [np.argsort(similarity[:,i])[:-k-1:-1]]
+                for j in xrange(ratings.shape[1]):
+                    pred[i, j] = similarity[i, :][top_k_users].dot(ratings[:, j][top_k_users]) 
+                    pred[i, j] /= np.sum(np.abs(similarity[i, :][top_k_users]))
+        if kind == 'item':
+            for j in xrange(ratings.shape[1]):
+                top_k_items = [np.argsort(similarity[:,j])[:-k-1:-1]]
+                for i in xrange(ratings.shape[0]):
+                    pred[i, j] = similarity[j, :][top_k_items].dot(ratings[i, :][top_k_items].T) 
+                    pred[i, j] /= np.sum(np.abs(similarity[j, :][top_k_items]))        
     
-return pred
+        return pred
 
 ## Reading data
 cols_names = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-#
+
+# modify to my own data path
+# using pandas to read ratings as a dataframe
 #ratings = pd.read_csv('./u.data', sep='\t', names=cols_names, encoding='latin-1')
 ratings = pd.read_csv('./collaborate-filter.data', sep='\t', names=cols_names, encoding='latin-1')
 
-
+# direccly access column, deduplicateion, get shape
+# http://pandas.pydata.org/pandas-docs/stable/tutorials.html
+# using ratings['user_id'] should be find access
+# https://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.unique.html
+# unique values
 n_users = ratings.user_id.unique().shape[0]
 n_items = ratings.movie_id.unique().shape[0]
 
 print str(n_users) + ' users | ' + str(n_items) + ' items'
 
 # Construct user-item matrix
-user_item_matrix = numpy.zeros((n_users, n_items))
+user_item_matrix = np.zeros((n_users, n_items))
 
 # Fill out the user-item matrix
+# iterative data frame by tuples
+# tuples start index is 1
+# https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.itertuples.html
 for row in ratings.itertuples():
+    # 0-1 value index,
+    # map from value to index 
+    # userid,
     user_item_matrix[row[1]-1, row[2]-1] = row[3]
 
-# Calculating sparcity: How many entries of the user_item_matrix are defined?
+# Calculating sparcity (in %): How many entries of the user_item_matrix are defined?
+# https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.nonzero.html
+# [0] : output row index
 sparsity = float(len(user_item_matrix.nonzero()[0]))
 sparsity /= (user_item_matrix.shape[0] * user_item_matrix.shape[1])
 sparsity *= 100
@@ -93,9 +143,12 @@ sparsity *= 100
 print 'Sparsity: {:4.2f}%'.format(sparsity)
 
 # We will split our data into training and test sets by removing 10 ratings per user from the training set and placing them in the test set.
+# ? can we user from sklearn.model_selection import train_test_split to do
+# probaly not becasue it cannot be selected row-based
 train, test = recommendation_helpers.train_test_split(user_item_matrix)
 
 # measure distance L2 between users
+# ? why needs to do this
 user_similarity = recommendation_helpers.calc_similarity(train, kind='user')
 
 numpy.savetxt('temp/user-similarity.txt', user_similarity, fmt='%f')
