e7e2390fb550703ee6026b873aa6ef2363edc1d1
Chieh-Chun Chang <say543@msn.com>
2019-04-05 23:04:23 -0700

    Create decisionTree_classification.py

---
+++ decisionTree_classification.py
@@ -0,0 +1,13 @@
+# not yet run through python environment
+
+import numpy as np
+
+import random
+from sklearn.datasets import make_blobs
+np.random.seed(123)
+
+# install matplotlib
+# this line for ipython mac only
+import matplotlib 
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
