50bdfc17db66665819d213b065850fd97f56ffb5
Chieh-Chun Chang <say543@msn.com>
2019-07-28 19:18:29 -0700

    Create SVC_ngram_classifer.py

---
+++ ngram_experiment/SVC_ngram_classifer.py
@@ -0,0 +1,5 @@
+##########
+# possilbe link
+# https://www.kaggle.com/tunguz/logistic-regression-with-words-and-char-n-grams/log
+# https://towardsdatascience.com/multi-class-text-classification-with-scikit-learn-12f1e60e0a9f
+############
